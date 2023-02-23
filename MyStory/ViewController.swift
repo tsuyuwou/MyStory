cabdca7323d28e37a8a16a2c5aa5d9b66e5bb6e4
Timmy Susai <timmytusharrajans@gmail.com>
2023-02-22 23:12:56 -0500

    Initial Commit

---
+++ MyStory/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyStory
+//
+//  Created by Tsuyuwou on 2/22/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
