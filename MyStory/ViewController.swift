4cb74a1d06204bf289b68f6b4fb23eac565813c6
Timmy Susai <timmytusharrajans@gmail.com>
2023-02-26 14:04:44 -0500

    Updated context item

--- MyStory/ViewController.swift
+++ MyStory/ViewController.swift
@@ -16,7 +16,7 @@ struct contextItem {
 class ViewController: UIViewController {
     
     let myDarkHistory = contextItem(name: "My Dark History", image: UIImage(named: "Ayanokouji")!, text: "Ironically, my dark history starts in the White Room. It is a secret facility that aims to raise a human from birth by removing unnecessary things from their education. Exposure to the outside world is forbidden and every aspect of their life growing up is controlled. Fortunately, the facility was temporarily shut down for a year which gave me the opportunity to escape.")
-    let myAbilities = contextItem(name: "My Abilities", image: UIImage(named: "Ayanokouji")!, text: "Martial Arts Mastery\n Stealth Mastery\n Social Influencing\n Fear-Inducing Aura\n Fear Manipulation\n Genius Intelligence\n Superhuman Speed\n Overpowering Strength\n Unyielding Durability")
+    let myAbilities = contextItem(name: "My Abilities", image: UIImage(named: "Ayanokouji")!, text: "Martial Arts Mastery\n Stealth Mastery\n Social Influencing\n Fear-Inducing Aura\n Fear Manipulation\n Genius Intelligence\n Superhuman Speed\n Overpowering Strength")
     let myPhilosophy = contextItem(name: "My Philosophy", image: UIImage(named: "Ayanokouji")!, text: "All people are nothing but tools. It doesn't matter how it's done. It doesn't matter what needs to be sacrificed. In this world, winning is everything. As long as I win in the end... That's all that matters.")
     var contextItems: [contextItem] = []
     
